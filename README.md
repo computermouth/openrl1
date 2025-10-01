ebbf8d293de5fdf4b3b8d4a1196671577f67f1ef
computermouth <computermouth@crunchbangplusplus.org>
2025-09-30 22:21:44 -0500

    Switch to bold, better logistics stuff for font script

--- README.md
+++ README.md
@@ -12,13 +12,17 @@ A LICENSE will be determined by contributing members, but it'll be something ext
 - Package and distribute the asset pack as a drop-in replacement for the original
 - Potentially package and distribute the game code + asset pack as a full game distributable ([more info here](https://github.com/flibitijibibo/RogueLegacy1/issues/2))
 
+## What is being worked on?
+
+I've just finished a working build process for the fonts. So far it only works for solid color ascii fonts. Check it out in `src/fonts`.
+
 ## What still needs to be done?
 
 There's a file at the root of this repository `files.txt` marking which files still have yet to be replaced, and what types the files are.
 
 ## How are we gonna replace those files?
 
-There's a few XNB packers/unpackers I found out there that can be used against to original data to get info like how the fonts and sprites are packed. Here's [a web-based one](https://lybell-art.github.io/xnb-js/).
+There's a few XNB packers/unpackers I found out there that can be used against to original data to get info like how the fonts and sprites are packed. Here's [a web-based one](https://lybell-art.github.io/xnb-js/). For the fonts I've been using [xnbcli](https://github.com/LeonBlade/xnbcli)
 
 ## Where can we organize?
 
--- src/fonts/render.sh
+++ src/fonts/render.sh
@@ -1,25 +1,29 @@
 #!/bin/bash -e
 
-if [[ ! -n $1 || ! -n $2 ]] ; then
+if [[ ! -n $1 || ! -n $2 || ! -n 3 || ! -n 4 ]] ; then
 	echo "E: bad args"
-	echo "   usage: $0 </path/to/font.ttf> <size>"
+	echo "   usage: $0 <font> <charset> <size> <output_name>"
 	exit 1
 fi
 
 FILE=$1
-SIZE=$2
+CHARSET=$2
+SIZE=$3
+OUTPUT=$4
 
 # glyph or cropping
 gorc_t='{"x": 0, "y": 0, "width": 0, "height": 0}'
 # kerning
 kern_t='{"x": 0, "y": 0, "z": 0}'
 
-template=$(cat template.json)
+REAL=$(dirname $(realpath $0))
+
+template=$(cat $REAL/template.json)
 
 height=-1
 width_marker=0
 
-out_name="$(basename $FILE)_$SIZE.png"
+out_name="$OUTPUT.png"
 json=$(echo $template | jq ".content.texture.export=\"$out_name\"")
 
 while IFS= read -r -n1 ch; do
@@ -45,13 +49,14 @@ while IFS= read -r -n1 ch; do
 	convert \
 		-background none \
 		-fill white \
-		-font $FILE \
+		-font $REAL/ttf/$FILE.ttf \
+		-antialias \
 		-pointsize $SIZE \
 		label:$prefix"$ch" \
-		png/$out.png
+		$REAL/png/$out.png
 	
-	h=$(identify -format "%h" png/$out.png)
-	w=$(identify -format "%w" png/$out.png)
+	h=$(identify -format "%h" $REAL/png/$out.png)
+	w=$(identify -format "%w" $REAL/png/$out.png)
 	
 	if [[ "$height" == "-1" ]]; then
 		height=$h
@@ -75,13 +80,16 @@ while IFS= read -r -n1 ch; do
 	
 	width_marker=$((width_marker + w))
 
-done < chars.txt
+done < $REAL/charset/$CHARSET.txt
 
 json=$(echo $json | jq ".content.verticalLineSpacing=$height")
 
 # build final texture
-LIST=$(find png/*.png)
-convert $LIST +append xnb/$out_name
+LIST=$(find $REAL/png/*.png)
+convert $LIST +append $REAL/xnb/$out_name
 
 # write out json
-echo $json | jq . > xnb/$(basename $FILE)_$SIZE.json
+echo $json | jq . > $REAL/xnb/$OUTPUT.json
+
+# build xnb
+xnbcli pack $REAL/xnb/$OUTPUT.json
